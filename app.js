0578290e845eb78f98b8afbe2ccf189ff4227696
MVendrik <35732059+MVendrik@users.noreply.github.com>
2020-11-06 14:41:07 +0100

    Cleanup

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const gravity = 0.9;
   let position = 0;
 
-
+  // Jump functionality Trex 
   document.addEventListener('keydown', (e) => {
     let count = 0;
     if (e.keyCode === 32) {
@@ -20,15 +20,15 @@ document.addEventListener('DOMContentLoaded', () => {
               if (count == 0 ){
                 clearInterval(timerDown);
                 isJumping = false;
-              }
-              count --;
+              };
+              count--;
               position -= 5;
               position = position * gravity;
               trex.style.bottom = position + 'px';
             }, 20);
-          }
+          };
   
-          count ++;
+          count++;
           position += 30;
           position = position * gravity;
           trex.style.bottom = position + 'px';
@@ -36,4 +36,5 @@ document.addEventListener('DOMContentLoaded', () => {
       };
     };
   } );
+
 });
