cdb5f78c009d61fefebc6b2608f5b327bccb6450
MVendrik <35732059+MVendrik@users.noreply.github.com>
2020-11-06 14:18:25 +0100

    Make T-rex div move up and down.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # t-rex-game
+
+The Dino Chrome Game, made with pure HTML, CSS and JavaScript.
--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// Script goes here
+document.addEventListener('DOMContentLoaded', () => {
+
+  const trex = document.querySelector('.trex');
+
+
+  document.addEventListener('keydown', (e) => {
+    let position = 0;
+    if (e.keyCode === 32) {
+    
+      let timerUp = setInterval( () => {
+        if (position == 150) {
+          clearInterval(timerUp);
+          let timerDown = setInterval( () => {
+            if (position == 30 ){
+              clearInterval(timerDown);
+            }
+            position -= 30;
+            trex.style.bottom = position + 'px';
+          }, 20);
+        }
+
+        position += 30;
+        trex.style.bottom = position + 'px';
+      }, 20);  
+    }
+    } );
+})
